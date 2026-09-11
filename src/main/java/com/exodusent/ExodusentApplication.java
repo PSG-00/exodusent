db947743f15c64fa93387c17429db705fd0dd763
PSG-00 <cdjsdj1902@gmail.com>
2026-09-11 23:56:22 +0900

    chore: initial project setup

---
+++ src/main/java/com/exodusent/ExodusentApplication.java
@@ -0,0 +1,13 @@
+package com.exodusent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExodusentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExodusentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exodusent/ExodusentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exodusent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExodusentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
